6f60a5f340432c2bd37d2f16230e7ea66837df75
rameshvajrapu <rameshvajrapu@gmail.com>
2021-10-11 16:29:01 +0530

    commit to main after switching

--- E2EPractise/src/test/java/Academy/E2EPractise/Registration.java
+++ E2EPractise/src/test/java/Academy/E2EPractise/Registration.java
@@ -61,7 +61,7 @@ public class Registration extends base {
 		r.mobilephone().sendKeys(Mobile);
 		r.alias().sendKeys(Alias);
 		r.registerbutton().click();
-		log.info("Registration Flow SuccessFull");
+		log.info("Registration Flow SuccessFull11102021");
 		
 		
 		
